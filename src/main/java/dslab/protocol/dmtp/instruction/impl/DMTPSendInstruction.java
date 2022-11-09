1a2dd61a349592bbc85a3812884ce4b84b08dd3b
smonm <e12021358@student.tuwien.ac.at>
2022-11-09 14:40:11 +0100

    reflect gitignore changes

---
+++ src/main/java/dslab/protocol/dmtp/instruction/impl/DMTPSendInstruction.java
@@ -0,0 +1,52 @@
+package dslab.protocol.dmtp.instruction.impl;
+
+import dslab.protocol.general.ExtendableErrorResponse;
+import dslab.protocol.general.SpecialErrorResponse;
+import dslab.protocol.general.instruction.Instruction;
+import dslab.protocol.general.instruction.impl.InstructionBase;
+import dslab.protocol.general.instruction.validator.ArgumentValidator;
+
+import java.util.List;
+
+/**
+ * A concrete implementation of {@code InstructionBase}.
+ *
+ * @see InstructionBase
+ */
+public final class DMTPSendInstruction extends InstructionBase
+        implements ExtendableErrorResponse, SpecialErrorResponse {
+
+    private boolean receivedSend;
+
+    public DMTPSendInstruction() {
+        this(null, null, null);
+    }
+
+    public DMTPSendInstruction(final List<ArgumentValidator> validators,
+                               final List<Instruction> requiredInstructions,
+                               final List<Instruction> dependentInstructions) {
+        super(validators, requiredInstructions, dependentInstructions);
+        this.receivedSend = false;
+    }
+
+    @Override
+    public String errorResponseExtension() {
+        return "no send";
+    }
+
+    @Override
+    public String handleInstructionHook(final String argument) {
+        receivedSend = true;
+        return okResponse();
+    }
+
+    @Override
+    public void resetHook() {
+        receivedSend = false;
+    }
+
+    @Override
+    public boolean isSet() {
+        return receivedSend;
+    }
+}
---
+++ src/main/java/dslab/protocol/dmtp/instruction/impl/DMTPSubjectInstruction.java
@@ -0,0 +1,56 @@
+package dslab.protocol.dmtp.instruction.impl;
+
+import dslab.protocol.general.ExtendableErrorResponse;
+import dslab.protocol.general.instruction.Instruction;
+import dslab.protocol.general.instruction.impl.InstructionBase;
+import dslab.protocol.general.instruction.validator.ArgumentValidator;
+
+import java.util.List;
+
+/**
+ * A concrete implementation of {@code InstructionBase}.
+ *
+ * @see InstructionBase
+ */
+public final class DMTPSubjectInstruction extends InstructionBase
+        implements ExtendableErrorResponse {
+
+    private String subject;
+
+    public DMTPSubjectInstruction() {
+        this(null, null, null);
+    }
+
+    public DMTPSubjectInstruction(final List<ArgumentValidator> validators,
+                                  final List<Instruction> requiredInstructions,
+                                  final List<Instruction> dependentInstructions) {
+        super(validators, requiredInstructions, dependentInstructions);
+        this.subject = null;
+    }
+
+    @Override
+    public String errorResponseExtension() {
+        return "no subject";
+    }
+
+    @Override
+    public String handleInstructionHook(final String argument) {
+        subject = argument.trim();
+        return okResponse();
+    }
+
+    @Override
+    public void resetHook() {
+        subject = null;
+    }
+
+    @Override
+    public boolean isSet() {
+        return subject != null;
+    }
+
+    @Override
+    public String getValue() {
+        return subject;
+    }
+}
